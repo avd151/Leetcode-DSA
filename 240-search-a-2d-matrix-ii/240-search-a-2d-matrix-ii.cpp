6350b17089c60d976a5268db2da086e4927b1557
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-07-24 06:50:23 +0530

    Time: 154 ms (60.80%), Space: 14.9 MB (68.84%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -17,19 +17,29 @@ public:
         // }
         
         //O(nlogm)
-        for(int i = 0; i < rows; i++){
-            int lo = 0;
-            int hi = cols-1;
-            while(lo <= hi){
-                int mid = lo + (hi-lo)/2;
-                if(matrix[i][mid] == target)
-                    return true;
-                else if(matrix[i][mid] > target)
-                    hi = mid-1;
-                else
-                    lo = mid+1;
-            }
+        // for(int i = 0; i < rows; i++){
+        //     int lo = 0;
+        //     int hi = cols-1;
+        //     while(lo <= hi){
+        //         int mid = lo + (hi-lo)/2;
+        //         if(matrix[i][mid] == target)
+        //             return true;
+        //         else if(matrix[i][mid] > target)
+        //             hi = mid-1;
+        //         else
+        //             lo = mid+1;
+        //     }
+        // }
+        int i = 0, j = cols-1; //stand at top rightmost element
+        while(i < rows && j >= 0){
+            if(matrix[i][j] == target)
+                return true;
+            else if(matrix[i][j] > target)
+                j--;
+            else 
+                i++;
         }
+        
         return false;
     }
 };
