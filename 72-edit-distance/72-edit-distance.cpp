8971946631526d308ca24bf834a7538dbf6a3c5e
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-07-26 05:37:16 +0530

    Time: 123 ms (13.93%), Space: 81.4 MB (6.99%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    int match(string s1, string s2, int i, int j, vector<vector<int>>& dp){
+        if(s1.length() == i)
+            return (s2.length()-j);
+        if(s2.length() == j)
+            return (s1.length()-i);
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        int res = 0;
+        if(s1[i] == s2[j])
+            res = match(s1, s2, i+1, j+1, dp);
+        else{
+            int insert = match(s1, s2, i, j+1, dp);
+            int delet = match(s1, s2, i+1, j, dp);
+            int replac = match(s1, s2, i+1, j+1, dp);
+            res = min({insert, delet, replac})+1;
+        }
+        return dp[i][j] = res;
+    }
+public:
+    int minDistance(string word1, string word2) {
+        int n1 = word1.length();
+        int n2 = word2.length();
+        if(n1 == 0)return n2;
+        if(n2 == 0)return n1;
+        vector<vector<int>>dp(n1, vector<int>(n2, -1));
+        return match(word1, word2, 0, 0, dp);
+    }
+};
