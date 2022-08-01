3c998d17ad2a781d1ecea33fe9f45e9e3eb07aa9
Apurva Deshpande <61773839+avd151@users.noreply.github.com>
2022-08-01 06:49:52 +0530

    Time: 3 ms (44.81%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,4 +1,5 @@
 class Solution {
+    //memoization - TC=O(n*m), SC=O(n*m + n+m))
     int f(int i, int j, vector<vector<int>>& dp){
         if(i == 0 && j == 0)
             return 1;
@@ -14,7 +15,30 @@ class Solution {
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n, -1));
-        return f(m-1, n-1, dp);
+        // vector<vector<int>>dp(m, vector<int>(n, -1));
+        // return f(m-1, n-1, dp);
+        
+        //tabulation TC=O(n*m), SC=O(n*m))
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+        // for(int i = 0; i <= m; i++)
+        //     dp[i][0] = 0;
+        // for(int j = 0; j <= n; j++)
+        //     dp[0][j] = 0;
+        // // dp[1][1] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                int up = 0, left = 0;
+                if(i==0 && j== 0)
+                    dp[i][j] = 1;
+                else{
+                    if(i >= 1)
+                        up += dp[i-1][j];
+                    if(j >= 1)
+                        left += dp[i][j-1];
+                    dp[i][j] = up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
